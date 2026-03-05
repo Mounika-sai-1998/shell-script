7e422aa6afbf1bddf6056f66209699389daba5a6
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 10:14:23 +0530

    Hello script

--- 06.array.sh
+++ 06.array.sh
@@ -2,6 +2,10 @@
 
 movies=("RRR" "A-aa" "F2")
 
+# list always starts with 0
+# size of the above array is 3
+# index of the above array is 2
+
 echo " fist movie is : ${movies[0]} "
 echo " second movie is : ${movies[1]} "
 echo " Third movie is : ${movies[2]} "

27da3ad6bf8c2e3633b226864b2cd472aa1d9636
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 12:57:10 +0530

    Hello script

--- 10.installations.sh
+++ 10.installations.sh
@@ -4,20 +4,30 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
-    echo " please run command with super user "
+    echo "please run command with super user "
     exit 1 # manually exists if error comes
 
 else 
-     echo " it is a super user "
+     echo "it is a super user "
 fi  
 
    dnf install mysqll -y
 
 if [ $? -ne 0 ]
 then 
-    echo " installation of mysql is failure..... "
+    echo "installation of mysql is failure..... "
     exit 2
+else 
+     echo "istallation of mysql is success...."
 
 fi
      dnf install git -y
+
+if [ $? -ne 0 ]
+then 
+     echo "installation of git is failed....."
+     exit 3 
+else
+     echo " istallation of git is success ...."
+fi
     

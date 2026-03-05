6383966ddf279ed350c96e4ea123bab3172f545a
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 09:59:47 +0530

    Hello script

---
+++ 06.array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+movies=("RRR" "A-AA" "F2")
+
+echo " fist movie is : {$movies[0]} "
+echo " second movie is : {$movies[1]} "
+echo " Third movie is : {$movies[2]} "
+echo " List all movies : {$movies[@]} "

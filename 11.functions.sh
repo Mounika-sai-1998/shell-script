632922277ca0f49b3fe2c25f01b13669e543dcb6
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 13:27:04 +0530

    Hello script

---
+++ 11.functions.sh
@@ -0,0 +1,26 @@
+#!/bn/bash
+
+USERID=$(id -u)
+
+validate(){
+    echo " existing status is : $1 "
+    echo " what are you doing : $2 "
+
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run command with super user "
+    exit 1 # manually exists if error comes
+
+else 
+     echo "it is a super user "
+fi 
+
+ validate $? "intallation" 
+
+   dnf install mysql -y
+   dnf install git -y
+
+
+    

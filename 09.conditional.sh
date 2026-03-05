2c80edec373e074de122737a0a0b5e1fe9df5cff
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 11:12:29 +0530

    Hello script

---
+++ 09.conditional.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER  -gt 10 ]
+then
+     echo " given number $NUMBER is greater than 10 "
+else 
+    echo " given number $NUMBER is lesser than 10 "
+fi
+
+
+# gt , lt , ge , le

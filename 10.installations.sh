c5fe73e0672da105b91b0aa6bf1f1624ca627a1c
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 12:52:51 +0530

    Hello script

--- 10.installations.sh
+++ 10.installations.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then 
     echo " please run command with super user "
-    exit 1
+    exit 1 # manually exists if error comes
 
 else 
      echo " it is a super user "
@@ -16,7 +16,7 @@ fi
 if [ $? -ne 0 ]
 then 
     echo " installation of mysql is failure..... "
-    exit 1
+    exit 2
 
 fi
      dnf install git -y

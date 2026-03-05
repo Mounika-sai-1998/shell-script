778e4f9fff4c1eb0f172a93f5f6c32bc47b484fd
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 10:02:40 +0530

    Hello script

--- 06.array.sh
+++ 06.array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-movies=("RRR" "Aaa" "Fida")
+movies=("RRR" "A-aa" "F2")
 
-echo " fist movie is : {$movies[0]} "
-echo " second movie is : {$movies[1]} "
-echo " Third movie is : {$movies[2]} "
-echo " List all movies : {$movies[@]} "
+echo " fist movie is : ${movies[0]} "
+echo " second movie is : ${movies[1]} "
+echo " Third movie is : ${movies[2]} "
+echo " List all movies : ${movies[@]} "

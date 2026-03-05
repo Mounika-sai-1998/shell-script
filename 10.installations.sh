7ae9725ecf000767d43b387ba1ee103cea5edf33
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 12:42:29 +0530

    Hello script

--- 10.installations.sh
+++ 10.installations.sh
@@ -1,10 +1,13 @@
 #!/bn/bash
 
-USERID=$[ id -u ]
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
     echo " please run command with super user "
+
+else 
+     echo " it is a super user "
 fi  
 
    dnf install mysql -y

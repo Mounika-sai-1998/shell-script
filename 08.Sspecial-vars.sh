4db8d4c02e78beba56a7ab3f37728c9cc055dc3c
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 10:44:40 +0530

    Hello script

--- 08.Sspecial-vars.sh
+++ 08.Sspecial-vars.sh
@@ -4,4 +4,6 @@ echo " All variables : $@ "
 echo " number of variables passed : $# "
 echo " Script name : $0 "
 echo " current working directory : $PWD "
-echo " Home directory of current User : $HOME "  
+echo " Home directory of current User : $HOME "
+echo " which user is running this script : $USER "
+  

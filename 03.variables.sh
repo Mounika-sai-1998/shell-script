712a1bde475b2ecccd3b82c137aefb4cc905d656
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-05 09:17:52 +0530

    Hello script

--- 03.variables.sh
+++ 03.variables.sh
@@ -2,12 +2,12 @@
 
 #DECLARING OS VARIABLES
 
-PERSON1=Ramesh   # there is no sapce b/w = and name
-PERSON2=Suresh
+PERSON1=Rahul  # there is no sapce b/w = and name
+PERSON2=Karthik
 
 # REFFERERING OF VARIABLES
 
 echo " $PERSON1 :: Hi $PERSON2 ,  how are you ? "
 echo " $PERSON2 :: Hello $PERSON1 , Iam fine . What about you ?"
 echo " $PERSON1 :: Yes, Iam Good . How The Things going on? "
-echo " $PERSON2:: Yeah ! Everything is going well..."
+echo " $PERSON2 :: Yeah ! Everything is going well..."

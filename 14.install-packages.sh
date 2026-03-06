d50bd01250ca98d2f67ffd4d4c5166587cb1bf71
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 10:20:02 +0530

    Hello script

--- 14.install-packages.sh
+++ 14.install-packages.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
+LOGFILE=/tmp/$SCRIPT_NAME/$IMESTAMP/
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 is sucesss..."
+    else
+        echo "$2 is failure...."
+}
 if [ $USERID -ne 0 ]
 then 
     echo "please run command with super user"
@@ -12,4 +22,10 @@ for i in $@
 do
     echo "package to install : $i"
     dnf list installed $i
+    if [ $? -eq 0 ]
+    then
+        echo "$i already installed ..... SKIPPING" &>>$LOGFILE
+    else
+        echo "$i not installed ...... need to install" &>>$LOGFILE
+
 done

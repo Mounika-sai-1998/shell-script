6a09d597f9c35911b0aa9b805c6810565fa498ff
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 08:17:00 +0530

    for installation

--- 14.install-packages.sh
+++ 14.install-packages.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-USERID=(id -u)
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+    echo "please run command with super user"
+else
+    echo "it is a super user"
+fi
 
- if [ $USERID -ne 0 ]
-
- then 
-     echo "please run command with super user"
- else
-     echo "it is a super user"
-
- fi
-
-
- for i in $@
-
- do
+for i in $@
+do
     echo "package to install : $i"
- done
+done

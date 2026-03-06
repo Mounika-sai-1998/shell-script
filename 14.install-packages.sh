11bb1bad5097c7b64c19c76abb673ea1a025dad9
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-06 10:36:51 +0530

    Hello script

--- 14.install-packages.sh
+++ 14.install-packages.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f2)
-LOGFILE=/tmp/$SCRIPT_NAME/$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
     if [ $1 -eq 0 ]
     then

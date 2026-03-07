63f7dcbb1a2ef41ca85e43b74debb9c62e753cd3
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-07 11:59:37 +0530

    expense -shell

--- 10.installations.sh
+++ 10.installations.sh
@@ -1,28 +1,26 @@
 #!/bn/bash
 
 USERID=$(id -u)
-
 if [ $USERID -ne 0 ]
 then 
     echo "please run command with super user "
     exit 1 # manually exists if error comes
-
 else 
      echo "it is a super user "
 fi  
 
-   dnf install mysql -y
 
+dnf install mysqlll -y
 if [ $? -ne 0 ]
 then 
     echo "installation of mysql is failure..... "
     exit 2
 else 
      echo "istallation of mysql is success...."
-
 fi
-     dnf install git -y
 
+
+dnf install git -y
 if [ $? -ne 0 ]
 then 
      echo "installation of git is failed....."

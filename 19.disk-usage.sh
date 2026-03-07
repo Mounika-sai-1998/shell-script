182ae0b447e317869ad11fa9c1fc1af876da70c3
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-07 21:32:46 +0530

    expense -shell

--- 19.disk-usage.sh
+++ 19.disk-usage.sh
@@ -5,12 +5,11 @@ DISK_THRESHOLD=75
 
 while IFS= read -r line
 do
-USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-FOLDER=$(df -hT | grep xfs | awk -F " " '{print $7F}')
+USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+FOLDER=$(echo $line  | awk -F " " '{print $7F}')
 if [ $USAGE -ge $DISK_THRESHOLD ]
 then
     echo " $FOLDER is more than $DISK_THRESHOLD . current usage : $USAGE "
 fi
-
 
 done <<< $DISK_USAGE

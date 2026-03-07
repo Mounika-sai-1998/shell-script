cde884b3cc3bcb8e6cb20803ff7a5ffb06693618
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-07 14:17:19 +0530

    expense -shell

--- 18.delete-old-logs.sh
+++ 18.delete-old-logs.sh
@@ -8,10 +8,10 @@ N="\e[0m"
 
 if [ -d $SOURCE_DIRECTORY ]
 then 
-    echo -e " $G source directoty exists $N"
+    echo -e "$G source directoty exists $N"
 else 
      echo -e "$R please make sure $SOURCE_DIRECTORY exists $N"
 fi
 
-FILES=(find . -name "*.log" -mtime +14)
-echo "Files tp delete : $SCRIPT_NAME"
+FILES=(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+echo "Files tp delete : $FILES"

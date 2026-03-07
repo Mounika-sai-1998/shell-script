c4314a3ca50d8d61502af7c9c74b8173e0320a5c
Mounika-sai-1998 <mounikaaarepu1998@gmail.com>
2026-03-07 12:04:04 +0530

    expense -shell

---
+++ 17.set.sh
@@ -0,0 +1,15 @@
+#!/bn/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+    echo "please run command with super user "
+    exit 1 # manually exists if error comes
+else 
+     echo "it is a super user "
+fi  
+
+dnf install mysql -y
+dnf install git -y
+
+echo " is script proceeding? "
